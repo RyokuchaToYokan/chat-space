260e2dfc8202e81ddae1a7e5741779094e9993a0
RyokuchaToYokan <autumnalbrook@gmail.com>
2020-04-27 21:34:15 +0900

    create group.new part2

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,22 @@
 class GroupsController < ApplicationController
 
 def new
+  @group = Group.new
+  @group.users << current_user  # ページ遷移時に自身の名前に自動的にチェックが入る
 end
 
-def create
+def create    #params=>  {"name"=>"グループ名", "user_ids"=>["", "2", "3"]}}
+  @group = Group.new(group_params)
+  if @group.save
+    redirect_to root_path, notice: "グループを作成しました"
+  else
+    render :new
+  end
+end
+
+private
+def group_params
+  params.require(:group).permit(:name, user_ids: [])
 end
 
 end
